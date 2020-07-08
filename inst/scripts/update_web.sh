342e5898da7b69f5aaa2a1ba77762fd2eb16caf3
Barry Rowlingson <b.rowlingson@lancs.ac.uk>
2020-07-08 18:01:07 +0100

    post-model git pushing

--- inst/scripts/update_web.sh
+++ inst/scripts/update_web.sh
@@ -5,18 +5,29 @@ if [ ! -d stsmap ] ; then
     exit -1
 fi
 
-
 if [ ! -d stsmodel ] ; then
     echo No stsmodel directory here in $PWD
     exit -1
 fi
 
 (cd stsmap; git pull)
-
 (cd stsmodel; git pull)
 
 ## run the daily update script in this docker container
 docker run -v $PWD/:/app/ -i -t stsmodel ./stsmap/inst/scripts/update_model.sh
 
-# now see if ./stsmodel/inst/days has changed, push if it has.
+# now see if ./stsmap/ has new files, push if it has.
 #...
+
+cd stsmap
+
+git status --porcelain | grep "^??"
+status=$?
+if [ $status = 1 ] ; then
+    echo no change
+else
+    git add inst/
+    git commit -a -m 'new data'
+    git push
+fi
+

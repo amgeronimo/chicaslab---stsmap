0e2bc6072f2fa10da49dd2530e055871887e431d
Barry Rowlingson <b.rowlingson@gmail.com>
2020-07-24 14:44:22 +0100

    removed code and put it into stsmodel

--- R/folders.R
+++
@@ -1,8 +0,0 @@
-
-folderFunction <- function(root){
-    n = normalizePath(root)
-    f = function(fp){
-        file.path(n, fp)
-    }
-    f
-}
---
+++ R/stsmap.R
@@ -0,0 +1,1 @@
+stsmap = function(){message("All this code is now in stsmodel and this is just a repo for gitlab pages")}

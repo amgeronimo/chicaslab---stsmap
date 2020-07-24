793d1c206fa750f8fb83a8341d408a757092e314
Barry Rowlingson <b.rowlingson@lancs.ac.uk>
2020-07-24 14:24:04 +0100

    checksum checking

--- R/model_and_map.R
+++ R/model_and_map.R
@@ -76,4 +76,55 @@ clean_up <- function(outputdir){
     unlink(od("data"),recursive=TRUE)
     
 }
+
+
+is_dir_metadata_different <- function(alldata, outputdir){
+    ## if the output directory doesn't exist then there's no checksums to test
+    if(!file.exists(outputdir)){
+        return(sm(TRUE, paste0("Output folder ",outputdir," does not exist")))
+    }
+
+    metadatafile = file.path(outputdir, "input_metadata.csv")
+    ## if there's no checksum metadata file then you should probably run the model
+    if(!file.exists(metadatafile)){
+        return(sm(TRUE, paste0("No input_metadata.csv file in ",outputdir)))
+    }
     
+    ##
+    previous = read.csv(file.path(outputdir, "input_metadata.csv"), stringsAsFactors=FALSE)
+    this = attr(alldata, "meta")
+    return(is_metadata_different(this, previous))
+}
+
+is_metadata_different <- function(alldata_meta, outputdir_meta){
+
+    ##
+    missing_this = setdiff(alldata_meta$name, outputdir_meta$name)
+    if(length(missing_this)>0){
+        return(sm(rerun=TRUE, paste0("Extra data items (",paste(missing_this,collapse=","), ") in new data")))
+    }
+    missing_prev <- setdiff(outputdir_meta$name, alldata_meta$name)
+    if(length(missing_prev) > 0){
+        return(sm(TRUE, paste0("Missing data items (",paste(missing_prev,collapse=","), ") in new data")))        
+    }
+    ## we should have the same number of rows in each data frame 
+    if(nrow(alldata_meta) != nrow(outputdir_meta)){
+        stop("New and old metadata tables have different number of rows")
+    }
+    
+    sorted_all = alldata_meta[order(alldata_meta$name),]
+    sorted_out = outputdir_meta[order(outputdir_meta$name),]
+    diffs = sorted_all$md5 != sorted_out$md5
+    if(any(diffs)){
+        return(sm(TRUE, paste0("Changed md5 checksums for ",paste(sorted_all$name[diffs],collapse=","))))
+    }
+    
+    return(sm(FALSE, "Data and md5 checksums match"))
+}
+
+sm = function(s, m, sn="status", mn="msg"){
+    ## sm = status message
+    ## combine two items into a list, typically the first will be a TRUE/FALSE status and
+    ## the second will be a message to go with that status
+    setNames(list(s,m), c(sn, mn))
+}
